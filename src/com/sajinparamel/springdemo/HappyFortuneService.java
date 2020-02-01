97a44d4fa37190f464e17e17f49c2d713e8d300c
Sajin Paramel <sajinparamel@gmail.com>
2020-02-01 16:09:14 +0530

    Fortune on random and Adding Properties file

--- src/com/sajinparamel/springdemo/HappyFortuneService.java
+++ src/com/sajinparamel/springdemo/HappyFortuneService.java
@@ -1,10 +1,16 @@
 package com.sajinparamel.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		
+		String fortuneArray[] = {"I don't think it's a good day","You are going to make a record","Luck is on your side"};
+		int fortuneIndex = new Random().nextInt(fortuneArray.length);
+		
+		return fortuneArray[fortuneIndex];
 	}
 
 }
